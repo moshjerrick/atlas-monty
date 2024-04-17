3f3c336537ba6c716ac0f20dddf7e383ddac299d
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-17 13:53:06 -0500

    added even more to header

--- monty.h
+++ monty.h
@@ -19,13 +19,19 @@ typedef struct instruction_s
 
 
 
-void op_push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_num);
-void push(stack_t **stack, unsigned int line_num, int n);
-void pop(stack_t **stack, unsigned int line_num);
+void pint(stack_t **stack, unsigned int line_num);
+void free_stack(stack_t *head);
+int pushint(char *list, int ln);
+int _strcmp(char *opcode, char *list);
+void add(stack_t **stack, unsigned int line_num);
 void swap(stack_t **stack, unsigned int line_num);
-
-
+void pop(stack_t **stack, unsigned int line_num);
+void push(stack_t **stack, unsigned int line_number, int n);
+void pall(stack_t **stack, unsigned int line_num);
+void execute(char *string[], stack_t *stack);
+int nlfind(char *list);
+void free_list(char *a[]);
+int combfind(char *list, int ln);
 
 
 #endif 
