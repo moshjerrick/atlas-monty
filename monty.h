aa53bcaf7076d44afaa356e8c6fb55a7605e35b4
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-17 13:44:54 -0500

    added prototypes to header

--- monty.h
+++ monty.h
@@ -5,6 +5,11 @@
 
 
 void op_push(stack_t **stack, unsigned int line_number);
+oid op_push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_num);
+void push(stack_t **stack, unsigned int line_num, int n);
+void pop(stack_t **stack, unsigned int line_num);
+void swap(stack_t **stack, unsigned int line_num);
 
 
 
