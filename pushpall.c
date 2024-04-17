8c8abe218a108a77e191aa7002a125324e34d56e
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-17 13:49:53 -0500

    double header!

--- pushpall.c
+++ pushpall.c
@@ -1,79 +1,66 @@
 #include "monty.h"
 #include <stdio.h>
-#include <stdio.h>
 #include <stdlib.h>
-
 
 void pall(stack_t **stack, unsigned int line_num)
 {
-	stack_t *h = *stack;
-	(void)line_num;
+    stack_t *h = *stack;
+    (void)line_num;
 
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
+    while (h)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+    }
 }
 
 void push(stack_t **stack, unsigned int line_num, int n)
 {
-	stack_t *new, *h = *stack;
+    stack_t *new, *h = *stack;
 
-	if (stack == NULL)
-	{
-		fprintf(stderr, "L%d: usage: push integer", line_num);
-		exit(EXIT_FAILURE);
-	}
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
-		exit(EXIT_FAILURE);
-	new->prev = NULL;
-	new->n = n;
-	new->next = *stack;
-	if (*stack)
-		h->prev = new;
-	*stack = new;
+    new = malloc(sizeof(stack_t));
+    if (new == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    new->prev = NULL;
+    new->n = n;
+    new->next = *stack;
+    if (*stack)
+        (*stack)->prev = new;
+    *stack = new;
 }
 
 void pop(stack_t **stack, unsigned int line_num)
 {
-	stack_t *h = *stack;
+    stack_t *h = *stack;
 
-	if (!(*stack))
-	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_num);
-		exit(EXIT_FAILURE);
-	}
+    if (!(*stack))
+    {
+        fprintf(stderr, "L%u: can't pop an empty stack\n", line_num);
+        exit(EXIT_FAILURE);
+    }
 
-
-	if (h)
-	{
-		*stack = (h)->next;
-		free(h);
-	}
+    *stack = (*stack)->next;
+    free(h);
 }
-
 
 void swap(stack_t **stack, unsigned int line_num)
 {
-	stack_t *h = *stack, *ptr;
+    stack_t *h = *stack, *ptr;
 
-	if ((*stack) == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%u: can't swap, stack too short\n", line_num);
-		exit(EXIT_FAILURE);
-	}
+    if ((*stack) == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't swap, stack too short\n", line_num);
+        exit(EXIT_FAILURE);
+    }
 
-	if (h && h->next)
-	{
-		ptr = h->next;
-		if (ptr->next)
-			ptr->next->prev = h;
-		h->next = ptr->next;
-		ptr->prev = NULL;
-		ptr->next = h;
-		h->prev = ptr;
-		*stack = ptr;
-	}
+    ptr = h->next;
+    if (ptr->next)
+        ptr->next->prev = h;
+    h->next = ptr->next;
+    ptr->prev = NULL;
+    ptr->next = h;
+    h->prev = ptr;
 }
