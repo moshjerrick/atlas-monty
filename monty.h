6aeae04cca0d8aa7729ce812232cafe93196ca50
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-17 13:47:09 -0500

    added to header

--- monty.h
+++ monty.h
@@ -1,10 +1,24 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
 
 
 
-void op_push(stack_t **stack, unsigned int line_number);
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_num);
+} instruction_t;
+
+
+
+
 void op_push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_num);
 void push(stack_t **stack, unsigned int line_num, int n);
