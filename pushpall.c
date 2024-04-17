e123fb8d806f1046774f6e883fb23f3a9c4c44d5
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-17 13:35:56 -0500

    changed output

--- pushpall.c
+++ pushpall.c
@@ -2,32 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define STACK_SIZE 100
+#define MAX_LINE_LENGTH 100
 
-int stack[STACK_SIZE];
-int top = -1;
-
-void push(int value, int line_number) {
-    if (top >= STACK_SIZE - 1) {
-        fprintf(stderr, "L%d: Error: Stack overflow\n", line_number);
-        exit(EXIT_FAILURE);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return EXIT_FAILURE;
     }
-    stack[++top] = value;
-}
 
-void pall() {
-    printf("Stack: ");
-    for (int i = top; i >= 0; i--) {
-        printf("%d ", stack[i]);
+    char *filename = argv[1];
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Unable to open file %s\n", filename);
+        return EXIT_FAILURE;
     }
-    printf("\n");
-}
 
-int main() {
-    char line[100];
+    char line[MAX_LINE_LENGTH];
     int line_number = 0;
 
-    while (fgets(line, sizeof(line), stdin) != NULL) {
+    while (fgets(line, sizeof(line), file) != NULL) {
         line_number++;
 
         if (line[strlen(line) - 1] == '\n') {
@@ -36,25 +29,17 @@ int main() {
 
         char *opcode = strtok(line, " ");
         if (opcode == NULL) {
-            continue; // Empty line
+            continue;
         }
 
-        if (strcmp(opcode, "push") == 0) {
-            char *arg = strtok(NULL, " ");
-            if (arg == NULL) {
-                fprintf(stderr, "L%d: Error: Usage: push integer\n", line_number);
-                exit(EXIT_FAILURE);
-            }
-            int value = atoi(arg);
-            push(value, line_number);
-        } else if (strcmp(opcode, "pall") == 0) {
-            pall();
-        } else {
-            fprintf(stderr, "L%d: Error: Unknown opcode: %s\n", line_number, opcode);
-            exit(EXIT_FAILURE);
+
+        printf("Line %d: Opcode: %s\n", line_number, opcode);
+        char *arg = strtok(NULL, " ");
+        if (arg != NULL) {
+            printf("Line %d: Argument: %s\n", line_number, arg);
         }
     }
 
-    return 0;
+    fclose(file);
+    return EXIT_SUCCESS;
 }
-
